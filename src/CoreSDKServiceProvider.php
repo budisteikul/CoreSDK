a5ae353f87f367cea43b4c124cb1d7f1c47b8d32
Budi <aku@budi.my.id>
2020-11-19 23:22:29 +0700

    #1891

--- src/CoreSDKServiceProvider.php
+++ src/CoreSDKServiceProvider.php
@@ -27,6 +27,7 @@ class CoreSDKServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/migrations/2020_11_17_000058_create_change_emails_table.php');
         $this->loadMigrationsFrom(__DIR__.'/migrations/2020_11_17_014602_create_file_temps_table.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations/2020_11_19_140324_create_cache_table.php');
         $this->publishes([ __DIR__.'/publish/assets' => public_path(''),], 'public');
     }
 }
