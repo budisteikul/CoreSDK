b26d4512b257bf326c1ac4b7dbfc1b4cfc2863c2
Budi <aku@budi.my.id>
2021-03-30 02:35:12 -0400

    #1891

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -20,7 +20,7 @@ class CorsMiddleware
     {
 
         return $next($request)
-        ->header('Access-Control-Allow-Headers', '*')
+        ->header('Access-Control-Allow-Headers', 'http://localhost:3000')
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
