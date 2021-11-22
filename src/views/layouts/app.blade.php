10b1408eb5543667a3ef61416fae958c4e94fcac
Budi <aku@budi.my.id>
2021-11-22 17:48:33 +0700

    remove version

--- src/views/layouts/app.blade.php
+++ src/views/layouts/app.blade.php
@@ -15,8 +15,8 @@
 
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <script src="{{ asset('js/admin-'.env('APP_VERSION').'.js') }}"></script>
-  <link href="{{ asset('css/admin-'.env('APP_VERSION').'.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/admin.js') }}"></script>
+  <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
   <link href="{{ asset('vendor/sbadmin2/sb-admin-2.css') }}" rel="stylesheet">
   
   @stack('scripts')
